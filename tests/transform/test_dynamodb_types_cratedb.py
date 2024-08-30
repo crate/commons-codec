3a31a06a6d389aa4cdd4e82df7cea28c4315d648
Andreas Motl <andreas@panodata.org>
2024-08-30 09:29:19 +0200

    DynamoDB: Add special decoding for varied lists
    Store them into a separate `OBJECT(IGNORED)` column in CrateDB.

---
+++ tests/transform/test_dynamodb_types_cratedb.py
@@ -0,0 +1,23 @@
+import unittest
+from decimal import Decimal
+
+from commons_codec.model import DualRecord
+from commons_codec.transform.dynamodb import CrateDBTypeDeserializer, DynamoDBCDCTranslator
+
+
+class TestDeserializer(unittest.TestCase):
+    def setUp(self):
+        self.deserializer = CrateDBTypeDeserializer()
+
+    def test_deserialize_list(self):
+        assert self.deserializer.deserialize({"L": [{"N": "1"}, {"S": "foo"}, {"L": [{"N": "1.25"}]}]}) == [
+            Decimal("1"),
+            "foo",
+            [Decimal("1.25")],
+        ]
+
+
+def test_decode_typed_untyped():
+    assert DynamoDBCDCTranslator(table_name="foo").decode_record(
+        {"foo": {"N": "84.84"}, "bar": {"L": [{"N": "1"}, {"S": "foo"}]}}
+    ) == DualRecord(typed={"foo": 84.84}, untyped={"bar": [1.0, "foo"]})
