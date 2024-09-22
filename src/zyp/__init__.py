47f59604d8ede5ab113db6d9155f30f4315a1251
Andreas Motl <andreas.motl@crate.io>
2024-09-22 10:44:05 +0200

    Zyp: Export main symbols via `zyp` namespace

---
+++ src/zyp/__init__.py
@@ -0,0 +1,17 @@
+from zyp.model import (
+    BucketTransformation,
+    CollectionTransformation,
+    FieldRenamer,
+    MokshaTransformation,
+    TransonTransformation,
+    ValueConverter,
+)
+
+__all__ = [
+    "CollectionTransformation",
+    "BucketTransformation",
+    "FieldRenamer",
+    "MokshaTransformation",
+    "TransonTransformation",
+    "ValueConverter",
+]
---
+++ src/zyp/model/__init__.py
@@ -0,0 +1,12 @@
+from zyp.model.bucket import BucketTransformation, FieldRenamer, TransonTransformation, ValueConverter
+from zyp.model.collection import CollectionTransformation
+from zyp.model.moksha import MokshaTransformation
+
+__all__ = [
+    "CollectionTransformation",
+    "BucketTransformation",
+    "FieldRenamer",
+    "MokshaTransformation",
+    "TransonTransformation",
+    "ValueConverter",
+]
